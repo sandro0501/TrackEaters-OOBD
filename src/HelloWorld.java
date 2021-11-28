e7c84faaf6b93a0cc821e7f40f36f6620c23bd1c
SantoloBarretta <san.barretta@studenti.unina.it>
2021-11-28 19:34:39 +0100

    commit di sandro

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,11 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao mondo we");
-<<<<<<< HEAD
-		System.out.print("Ciaosandro");
-=======
-		System.out.print("12345");
->>>>>>> 778db8b2ddbf13b53f2e1cd94fcad448d5aa9562
+
+		System.out.print("Ciaosanedro");
+
+		System.out.print("12rewrwe345");
+
 
 	}
 

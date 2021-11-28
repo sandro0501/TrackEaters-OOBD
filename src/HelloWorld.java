7d347c3a7374548aca8f3ab24b2c419125ca0303
Angelo <angelo.dimaio3@studenti.unina.it>
2021-11-28 19:00:06 +0100

    Aggiunta riga

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao mondo we");
+		System.out.print("WEWE");
 
 	}
 

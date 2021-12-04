bb5cb4388516c33065821c8688ec1d2dcb5c7ec6
SantoloBarretta <san.barretta@studenti.unina.it>
2021-12-04 02:46:13 +0100

    Aggiunta connessione al DB con JDBC

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ciao mondo we");
+		System.out.println("Ciao");
 
-		System.out.print("Ciaosanedrerewro");
-		System.out.print("12rewrwtrhtrhe345");
 
 
 

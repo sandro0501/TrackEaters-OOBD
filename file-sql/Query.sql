08bf479fe1d3fdd23cba64b77aebcae59814e9d9
SantoloBarretta <san.barretta@studenti.unina.it>
2022-01-26 01:05:27 +0100

    aggiunte query

--- file-sql/Query.sql
+++ file-sql/Query.sql
@@ -1,20 +1,19 @@
 -- QUERY DA EFFETTUARE SUL DB 
-
--- Query 1: 
+ 
 /* Numero giornaliero totale di avventori per ristorante	
 La query, dato un determinato ristorante e specificata una data, 
 calcola il numero giornaliero totale di avventori 
 che sono stati accolti dal ristorante in quella data.
 Supponiamo che il ristorante dato abbia CodRistorante=1 e la DataArrivo='17/11/2021'. */
 
-SELECT 	COUNT(A.NumCid) AS numero_giornaliero_avventori
-FROM 	Avventore A JOIN Tavolata T ON A.Tavolata=T.CodTavolata 
-WHERE 	A.Ristorante=1 AND T.DataArrivo=TO_DATE('17/11/2021', 'dd/mm/yyyy');
+SELECT T.DataArrivo AS DATA, COUNT(A.NumCid) AS TOT_GIORNALIERO_AVVENTORI
+FROM Avventore A JOIN Tavolata T ON A.Tavolata = T.CodTavolata 
+WHERE A.Ristorante = 1 AND T.DataArrivo = TO_DATE('17/11/2021', 'dd/mm/yyyy')
+GROUP BY T.DataArrivo;
 
 /*============================================================================================*/
 /*============================================================================================*/
 
--- Query 2: 
 /* Numero mensile totale di avventori per ristorante
 La query, dato un determinato ristorante e specificato un mese, 
 calcola il numero mensile totale di avventori che sono stati accolti dal ristorante in quel mese. */
@@ -27,7 +26,6 @@ GROUP BY TO_CHAR(T.DataArrivo, 'yyyy'), TO_CHAR(T.DataArrivo, 'mm');
 /*============================================================================================*/
 /*============================================================================================*/
 
---Query 3: 
 /* Numero giornaliero totale di avventori per tutti i ristoranti di un proprietario	
 La query, dato un determinato proprietario e specificata una data, 
 calcola il numero giornaliero totale di avventori che sono stati accolti da tutti i ristoranti 
@@ -40,11 +38,9 @@ FROM    (RISTORANTE R JOIN AVVENTORE A
 WHERE   R.Proprietario=1 AND T.DataArrivo=TO_DATE('22/11/2021', 'dd/mm/yyyy')
 GROUP   BY R.CodRistorante; 
 
-
 /*============================================================================================*/
 /*============================================================================================*/
 
---Query 4: 
 /* Numero mensile totale di avventori per tutti i ristoranti 
 di un proprietario	La query, dato un determinato proprietario 
 e specificato un mese, calcola il numero mensile totale di avventori 
@@ -62,3 +58,75 @@ GROUP   BY R.CodRistorante;
 /*============================================================================================*/
 
 --Query 5:
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+-- Casi positivi mensili per tutti i ristoranti di un proprietario
+SELECT TO_CHAR(C.DataRegistrazione,'mm  ') AS MESE, TO_CHAR(C.DataRegistrazione,'yyyy') AS ANNO, COUNT(C.CodCaso) AS TOT_AVVENTORI_POSITIVI
+FROM Caso C JOIN Avventore A ON C.AvventorePositivo = A.NumCid JOIN Ristorante R ON R.CodRistorante = A.Ristorante
+WHERE R.Proprietario = 1 AND TO_CHAR(C.DataRegistrazione,'mm')= 01 AND TO_CHAR(C.DataRegistrazione,'yyyy')= 2022
+GROUP BY TO_CHAR(C.DataRegistrazione,'mm  '), TO_CHAR(C.DataRegistrazione,'yyyy');
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+-- Casi positivi annuali per tutti i ristoranti di un proprietario
+SELECT TO_CHAR(C.DataRegistrazione,'yyyy') AS ANNO, COUNT(C.CodCaso) AS TOT_AVVENTORI_POSITIVI
+FROM Caso C JOIN Avventore A ON C.AvventorePositivo = A.NumCid JOIN Ristorante R ON R.CodRistorante = A.Ristorante
+WHERE R.Proprietario = 1 AND TO_CHAR(C.DataRegistrazione,'yyyy')= 2022
+GROUP BY TO_CHAR(C.DataRegistrazione,'yyyy');
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+-- Informazioni sugli avventori risultati positivi in un ristorante
+SELECT C.CodCaso, C.DataRegistrazione AS DATAR, A.NumCid, CAST(A.Nome AS VARCHAR2(30)) AS Nome, CAST(A.Cognome AS VARCHAR2(30)) AS Cognome, 
+A.DataN, A.Telefono, A.HaGreenpass AS G , T.CodTavolata, T.DataArrivo, T.Cameriere, T.Tavolo, TA.TavoloAdiacente 
+FROM CASO C JOIN AVVENTORE A ON C.AvventorePositivo = A.NumCid JOIN TAVOLATA T ON T.CodTavolata = A.Tavolata JOIN TAVOLO TA ON TA.CodTavolo = T.Tavolo
+WHERE A.Ristorante = 1;
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+-- Informazioni sui camerieri risultati positivi in un ristorante
+SELECT C.CodCaso, C.DataRegistrazione AS DATAR, C.StatoCaso, CA.NumCid, CA.Nome, CA.Cognome, CA.DataN, CA.Sesso, CA.Telefono
+FROM CAMERIERE CA JOIN CASO C ON CA.NumCid = C.CamerierePositivo
+WHERE CA.Ristorante = 1;
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+--Avventori positivi con o senza green pass 
+SELECT COUNT(C.CodCaso) AS POSITIVI_CON_GREENPASS
+FROM CASO C JOIN AVVENTORE A ON C.AvventorePositivo = A.NumCid 
+WHERE A.HaGreenpass='V';
+
+SELECT COUNT(C.CodCaso) AS POSITIVI_SENZA_GREENPASS
+FROM CASO C JOIN AVVENTORE A ON C.AvventorePositivo = A.NumCid 
+WHERE A.HaGreenpass='F';
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+--Numero di avventori medio per tavolata di un ristorante
+--1.Si crea la vista "Tavolate" che per ogni tavolata di un ristorante calcola il numero totale di partecipanti alla tavolata. 
+CREATE VIEW TAVOLATE (Ristorante, CodiceTavolata, Partecipanti) AS
+SELECT A.Ristorante, A.Tavolata, COUNT(A.NumCid) AS TOT_TAVOLATA
+FROM AVVENTORE A JOIN TAVOLATA T ON A.Tavolata = T.CodTavolata
+GROUP BY A.Ristorante, A.Tavolata
+ORDER BY A.Tavolata ASC;
+--2.Si esegue la seguente query sulla vista, 
+-- supponendo di voler conoscere la media di avventori per tavolata del ristorante di CodRistorante = 2; 
+SELECT T.Ristorante, AVG(T.Partecipanti) AS MEDIA_AVVENTORI_PER_TAVOLATA
+FROM TAVOLATE T
+WHERE T.Ristorante = 2;
+GROUP BY T.Ristorante;
+
+/*============================================================================================*/
+/*============================================================================================*/
+
+-- Avventori e camerieri risultati positivi in una stessa tavolata di un ristorante 
+
+/*============================================================================================*/
+/*============================================================================================*/

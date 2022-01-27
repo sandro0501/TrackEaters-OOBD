241bec469798693d908f489a09f6e529c371524a
SantoloBarretta <san.barretta@studenti.unina.it>
2022-01-27 12:58:10 +0100

    screenshot tabelle

--- Servizi Start & Stop/DBStart.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-net start OracleOraDB19Home1MTSRecoveryService
-net start OracleOraDB19Home1TNSListener
-net start OracleVssWriterORCL
-net start OracleServiceORCL
-echo Tutti i servizi sono stati avviati correttamente
-pause
--- Servizi Start & Stop/DBStop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-net STOP OracleOraDB19Home1MTSRecoveryService
-net STOP OracleOraDB19Home1TNSListener
-net STOP OracleVssWriterORCL
-net STOP OracleServiceORCL
-echo Tutti i servizi sono stati arrestati correttamente
-pause

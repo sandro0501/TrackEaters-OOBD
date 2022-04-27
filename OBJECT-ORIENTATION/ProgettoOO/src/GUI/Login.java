abfda8bd7081d068b0adf01384806199e6b8e3e2
SantoloBarretta <san.barretta@studenti.unina.it>
2022-04-27 16:04:50 +0200

    primo commit progetto oo

---
+++ OBJECT-ORIENTATION/ProgettoOO/src/GUI/Login.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

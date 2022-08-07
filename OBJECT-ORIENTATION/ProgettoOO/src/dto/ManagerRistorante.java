075533049e9cdf3c5c5187fe12b1c0da95cef76e
santolobarretta <san.barretta@studenti.unina.it>
2022-08-08 00:37:02 +0200

    implementazione classe ManagerRistorante v.1.0

--- OBJECT-ORIENTATION/ProgettoOO/src/dto/ManagerRistorante.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dto/ManagerRistorante.java
@@ -1,5 +1,32 @@
 package dto;
 
-public class ManagerRistorante {
+public class ManagerRistorante extends Operatore {
+	
+	private String telefono;
+	
+	private Ristorante ristoranteGestito;
+	
+	public ManagerRistorante(String username, String password, String nome, String cognome, String email, String telefono) {
+		super(username, password, nome, cognome, email);
+		this.setTelefono(telefono);
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Ristorante getRistoranteGestito() {
+		return ristoranteGestito;
+	}
+
+	public void setRistoranteGestito(Ristorante ristoranteGestito) {
+		this.ristoranteGestito = ristoranteGestito;
+	}
+
+	
 
 }

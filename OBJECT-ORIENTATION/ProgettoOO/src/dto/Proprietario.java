f5cc0811053ff508ba8f18a2acfd918899ffa027
santolobarretta <san.barretta@studenti.unina.it>
2022-08-08 00:38:02 +0200

    implementazione classe Proprietario v.1.0

--- OBJECT-ORIENTATION/ProgettoOO/src/dto/Proprietario.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dto/Proprietario.java
@@ -1,5 +1,21 @@
 package dto;
 
-public class Proprietario {
+import java.util.List;
 
+public class Proprietario extends Operatore {
+	
+	private List<Ristorante> ristorantiAmministrati;
+
+	public Proprietario(String username, String password, String nome, String cognome, String email) {
+		super(username, password, nome, cognome, email);
+	}
+
+	public List<Ristorante> getRistorantiAmministrati() {
+		return ristorantiAmministrati;
+	}
+
+	public void setRistorantiAmministrati(List<Ristorante> ristorantiAmministrati) {
+		this.ristorantiAmministrati = ristorantiAmministrati;
+	}
+	
 }

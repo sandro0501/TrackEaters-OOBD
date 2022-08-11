0f6e4f3f628cc652aeac7e7cdedbcfeddb9cda95
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-11 21:27:36 +0200

    modifiche minimali

--- OBJECT-ORIENTATION/ProgettoOO/src/gui/Homepage_Proprietario.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/gui/Homepage_Proprietario.java
@@ -37,6 +37,7 @@ public class Homepage_Proprietario extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Homepage_Proprietario.class.getResource("/resources/icon.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1060, 500);
+		setLocationRelativeTo(null);
 		pannello_Principale = new JPanel();
 		pannello_Principale.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(pannello_Principale);

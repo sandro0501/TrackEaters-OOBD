b6b1d2d3831491551fd6f12f048df7c9aa8045a4
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-17 01:31:17 +0200

    implementazione metodi proprietariodao v.1.0

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/ProprietarioDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/ProprietarioDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
 public interface ProprietarioDAO {
+	
+	boolean updateProprietario(String currentUsername, String nome, String cognome, String newUsername, String email, String password);
 
 }

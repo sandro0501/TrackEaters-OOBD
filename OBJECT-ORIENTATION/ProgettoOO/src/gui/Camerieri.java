673999341458fb2ebd307c2b0309ca24386b714a
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-19 03:27:33 +0200

    cambiato nome frame

--- OBJECT-ORIENTATION/ProgettoOO/src/gui/Camerieri.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/gui/Camerieri.java
@@ -36,7 +36,7 @@ public class Camerieri extends JFrame {
 		
 		theController = c;
 		
-		setIconImage(Toolkit.getDefaultToolkit().getImage(Ristoranti.class.getResource("/resources/icon.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RistorantiProprietarioFrame.class.getResource("/resources/icon.png")));
 		setTitle("SecuRisto");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

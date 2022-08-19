9ac7dd39e75ed80b8cc8903af3af173c892bbb9c
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-19 03:27:16 +0200

    cambiato nome frame

---
+++ OBJECT-ORIENTATION/ProgettoOO/src/gui/RistorantiProprietarioFrame.java
@@ -0,0 +1,227 @@
+package gui;
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JTable;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EtchedBorder;
+import java.awt.Cursor;
+import javax.swing.JScrollBar;
+import java.awt.Toolkit;
+import controller.Controller;
+import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+
+public class RistorantiProprietarioFrame extends JFrame {
+
+	private JPanel pannello_Principale;
+	private JTable tabella_Ristoranti;
+	private Controller theController;
+	private JTable table;
+	private JLabel lblDataEOra;
+
+	public RistorantiProprietarioFrame(Controller c) {
+		
+		theController=c;
+		
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RistorantiProprietarioFrame.class.getResource("/resources/icon.png")));
+		setTitle("TrackEaters - I miei ristoranti");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		setLocationRelativeTo(null);
+		pannello_Principale = new JPanel();
+		pannello_Principale.setBackground(new Color(176, 196, 222));
+		pannello_Principale.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pannello_Principale);
+		pannello_Principale.setLayout(null);
+		
+		JLabel etichetta_IMieiRistoranti = new JLabel("");
+		etichetta_IMieiRistoranti.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/lblIMieiRistoranti.png")));
+		etichetta_IMieiRistoranti.setFont(new Font("Tahoma", Font.BOLD, 20));
+		etichetta_IMieiRistoranti.setHorizontalAlignment(SwingConstants.CENTER);
+		etichetta_IMieiRistoranti.setBounds(387, 45, 490, 52);
+		pannello_Principale.add(etichetta_IMieiRistoranti);
+		
+		
+		
+		JButton bottone_Gestisci = new JButton("");
+		bottone_Gestisci.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnGestisci.png")));
+		bottone_Gestisci.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tabella_Ristoranti.getSelectedRow()!=-1) {
+					setVisible(false);
+					c.startRistorante(true);
+				} else {
+					JOptionPane.showMessageDialog(pannello_Principale, "Nessun ristorante selezionato!");
+				}
+			}
+		});
+		bottone_Gestisci.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Gestisci.setBounds(124, 503, 160, 60);
+		pannello_Principale.add(bottone_Gestisci);
+		
+		JButton bottone_Aggiungi = new JButton("");
+		bottone_Aggiungi.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnAggiungi.png")));
+		bottone_Aggiungi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					c.startAggiungiRistorante();
+			}
+		});
+		bottone_Aggiungi.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Aggiungi.setBounds(408, 503, 160, 60);
+		pannello_Principale.add(bottone_Aggiungi);
+		
+		JButton bottone_Modifica = new JButton("");
+		bottone_Modifica.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnModifica.png")));
+		bottone_Modifica.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tabella_Ristoranti.getSelectedRow()!=-1) {
+					setVisible(false);
+					c.startModificaRistorante();
+				} else {
+					JOptionPane.showMessageDialog(pannello_Principale, "Nessun ristorante selezionato!");
+				}
+				
+			}
+		});
+		bottone_Modifica.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Modifica.setBounds(692, 503, 160, 60);
+		pannello_Principale.add(bottone_Modifica);
+		
+		JButton bottone_Elimina = new JButton("");
+		bottone_Elimina.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnElimina.png")));
+		bottone_Elimina.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tabella_Ristoranti.getSelectedRow()!=-1) {
+					JOptionPane.showConfirmDialog(pannello_Principale, "Sei sicuro di voler eliminare il ristorante selezionato?");
+				} else {
+					JOptionPane.showMessageDialog(pannello_Principale, "Nessun ristorante selezionato!");
+				}
+				
+			}
+		});
+		bottone_Elimina.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Elimina.setBounds(976, 503, 160, 60);
+		pannello_Principale.add(bottone_Elimina);
+		
+		JPanel pannello_Navigazione = new JPanel();
+		pannello_Navigazione.setLayout(null);
+		pannello_Navigazione.setForeground(new Color(0, 0, 128));
+		pannello_Navigazione.setBorder(new LineBorder(new Color(119, 136, 153), 2));
+		pannello_Navigazione.setBackground(new Color(176, 196, 222));
+		pannello_Navigazione.setBounds(0, 597, 1264, 84);
+		pannello_Principale.add(pannello_Navigazione);
+		
+		JButton bottone_Indietro = new JButton("");
+		bottone_Indietro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				c.startHomepageProprietario();
+			}
+		});
+		bottone_Indietro.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnIndietro.png")));
+		bottone_Indietro.setFont(new Font("Tahoma", Font.BOLD, 12));
+		bottone_Indietro.setBounds(191, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Indietro);
+		
+		JButton bottone_Logout = new JButton("");
+		bottone_Logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostraLogoutDialog(c);
+			}
+		});
+		bottone_Logout.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnLogout.png")));
+		bottone_Logout.setFont(new Font("Tahoma", Font.BOLD, 12));
+		bottone_Logout.setBounds(1094, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Logout);
+		
+		JButton bottone_Home = new JButton("");
+		bottone_Home.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				c.startHomepageProprietario();
+			}
+		});
+		bottone_Home.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/btnHome.png")));
+		bottone_Home.setFont(new Font("Tahoma", Font.BOLD, 12));
+		bottone_Home.setBounds(10, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Home);
+		
+		lblDataEOra = new JLabel("");
+		lblDataEOra.setIcon(new ImageIcon(RistorantiProprietarioFrame.class.getResource("/resources/DataEOraIcon.png")));
+		lblDataEOra.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDataEOra.setForeground(new Color(0, 0, 128));
+		lblDataEOra.setFont(new Font("Segoe UI", Font.BOLD, 25));
+		lblDataEOra.setBounds(420, 11, 429, 60);
+		pannello_Navigazione.add(lblDataEOra);
+		this.mostraDataEOra();
+		
+		JLabel lblUsername = new JLabel("");
+		lblUsername.setForeground(new Color(0, 0, 128));
+		lblUsername.setFont(new Font("Segoe UI", Font.BOLD, 22));
+		lblUsername.setBounds(10, 11, 274, 40);
+		pannello_Principale.add(lblUsername);
+		
+		table = new JTable();
+		table.setBounds(26, 126, 1211, 348);
+		pannello_Principale.add(table);
+		
+		
+	}
+	
+	public void mostraDataEOra() {
+		Thread clock = new Thread() {
+			public void run() {
+				try {
+					for(;;) {
+						Calendar cal = new GregorianCalendar();
+						int giorno = cal.get(Calendar.DAY_OF_MONTH);
+						int mese = cal.get(Calendar.MONTH)+1;
+						int anno = cal.get(Calendar.YEAR);
+						
+						int secondi = cal.get(Calendar.SECOND);
+						int minuti = cal.get(Calendar.MINUTE);
+						int ore = cal.get(Calendar.HOUR_OF_DAY);
+						
+						lblDataEOra.setText(giorno+"/"+mese+"/"+anno+" - "+ore+":"+minuti+":"+secondi);
+						sleep(1000);
+					}
+				} catch (InterruptedException e) {
+					
+				}
+			}
+		};
+		clock.start();
+	}
+	
+	private void mostraLogoutDialog(Controller c) {
+		JLabel lblLogout = new JLabel("Sei sicuro di voler uscire?");
+		lblLogout.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		if(JOptionPane.showConfirmDialog(pannello_Principale, lblLogout)==0) {
+			setVisible(false);
+			c.startLogin();
+		}
+	}
+}

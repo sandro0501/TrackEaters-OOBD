27681ad8c19f67a978b48407cb61324583dbdd89
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-20 18:15:09 +0200

    aggiunto metodo dao getRistorantiByUsernameProprietario

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import java.util.ArrayList;
+
+import dto.Ristorante;
+
 public interface RistoranteDAO {
+	
+	 ArrayList<Ristorante> getRistorantiByUsernameProprietario(String usernameProprietario);
 
 }

978a3b43e0dbca11ab727821346311b2a55643c0
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-22 20:11:41 +0200

    aggiunti nuovi metodi dao (getCodProprietario)

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/ProprietarioDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/ProprietarioDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 public interface ProprietarioDAO {
 	
+	int getCodiceProprietarioFromUsername(String usernameProprietario);
 	boolean updateProprietario(String currentUsername, String nome, String cognome, String newUsername, String email, String password);
+	
 
 }

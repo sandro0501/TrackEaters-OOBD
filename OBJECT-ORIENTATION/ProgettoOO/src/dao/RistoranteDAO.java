9ab1fcb4c9a330a7c8d54c3a9d95017dcf9900df
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-24 17:45:07 +0200

    implementazione metodo dao deleteRistorante

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
@@ -8,5 +8,6 @@ public interface RistoranteDAO {
 	
 	 ArrayList<Ristorante> getRistorantiByUsernameProprietario(String usernameProprietario);
 	 boolean insertRistorante(String denominazione, String indirizzo, String telefono, String citta, String prov, String cap, String email, String sitoweb, int proprietario);
+	 boolean deleteRistorante(String denominazione);
 	 
 }

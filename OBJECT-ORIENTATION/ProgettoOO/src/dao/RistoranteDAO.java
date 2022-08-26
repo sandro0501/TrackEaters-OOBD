ca5537acb6152026bbd525bc4428dd6a108d9706
SantoloBarretta <san.barretta@studenti.unina.it>
2022-08-26 23:58:47 +0200

    definito metodo update ristorante dao

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/RistoranteDAO.java
@@ -7,7 +7,8 @@ import dto.Ristorante;
 public interface RistoranteDAO {
 	
 	 ArrayList<Ristorante> getRistorantiByUsernameProprietario(String usernameProprietario);
+	 int getCodiceRistoranteByDenominazioneAndIndirizzo(String denominazione, String indirizzo);
 	 boolean insertRistorante(String denominazione, String indirizzo, String telefono, String citta, String prov, String cap, String email, String sitoweb, int proprietario);
-	 boolean deleteRistorante(String denominazione);
-	 
+	 boolean updateRistorante(int codRistorante, String denominazione, String indirizzo, String telefono, String citta, String prov, String cap, String email, String sitoweb);
+	 boolean deleteRistorante(int codRistorante); 
 }

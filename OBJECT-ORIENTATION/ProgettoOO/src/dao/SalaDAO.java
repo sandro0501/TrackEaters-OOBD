4247961f077315a8cab96dda1907efc7618ae47c
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-05 00:02:37 +0200

    aggiunti metodi DAO (insert,update,delete,getCodSala)

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/SalaDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/SalaDAO.java
@@ -7,4 +7,8 @@ import dto.Sala;
 public interface SalaDAO {
 	
 	 ArrayList<Sala> getSaleByCodRistorante(int codRistorante);
+	 int getCodiceSalaByDenominazioneAndCapienza(String denominazione, int capienza);
+	 boolean insertSala(String denominazione, int capienza, int dimensioneMq, String tipologia, int ristorante);
+	 boolean updateSala(int codSala, String denominazione, int capienza, int dimensioneMq, String tipologia);
+	 boolean deleteSala(int codSala); 
 }

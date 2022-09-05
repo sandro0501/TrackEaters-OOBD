1076fd3faea5a9dc823b1cb4432e71d5dbf79d57
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-06 00:31:11 +0200

    aggiunto metodo dao getSalaByCodice

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/SalaDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/SalaDAO.java
@@ -8,6 +8,7 @@ public interface SalaDAO {
 	
 	 ArrayList<Sala> getSaleByCodRistorante(int codRistorante);
 	 int getCodiceSalaByDenominazioneAndCapienza(String denominazione, int capienza);
+	 Sala getSalaByCodice(int codSala);
 	 boolean insertSala(String denominazione, int capienza, int dimensioneMq, String tipologia, int ristorante);
 	 boolean updateSala(int codSala, String denominazione, int capienza, int dimensioneMq, String tipologia);
 	 boolean deleteSala(int codSala); 

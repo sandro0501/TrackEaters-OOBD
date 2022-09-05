6831123953c250536a1ae1837d5a74ec7ce6e95a
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-06 00:31:30 +0200

    aggiunti metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/TavoloDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/TavoloDAO.java
@@ -1,5 +1,13 @@
 package dao;
 
+import java.util.ArrayList;
+
+import dto.Tavolo;
+
 public interface TavoloDAO {
 
+	ArrayList<Tavolo> getTavoliByCodSala(int codSala);
+	boolean insertTavolo(int codTavolo, int maxAvventori, int codSala);
+	boolean updateTavolo(int maxAvventori);
+	boolean deleteTavolo(int codTavolo); 
 }

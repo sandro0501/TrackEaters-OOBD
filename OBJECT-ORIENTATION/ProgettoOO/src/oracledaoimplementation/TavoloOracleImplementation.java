bb93ab502c5812e95376754974c72c90c205b52d
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-07 02:23:35 +0200

    implementazione metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/oracledaoimplementation/TavoloOracleImplementation.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/oracledaoimplementation/TavoloOracleImplementation.java
@@ -1,10 +1,14 @@
 package oracledaoimplementation;
 
+import java.awt.Font;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import dao.TavoloDAO;
 import database.ConnessioneDatabase;
@@ -51,20 +55,88 @@ public class TavoloOracleImplementation implements TavoloDAO {
 	}
 
 	@Override
-	public boolean insertTavolo(int codTavolo, int maxAvventori, int codSala) {
-		// TODO Auto-generated method stub
+	public boolean insertTavolo(int maxAvventori, int codSala) {
+		try {
+			String queryInsertTavolo = "INSERT INTO TAVOLO (Maxavventori,Sala) VALUES (?,?)";
+			PreparedStatement insertTavoloStatement = connessione.prepareStatement(queryInsertTavolo);
+			insertTavoloStatement.setInt(1,maxAvventori);
+			insertTavoloStatement.setInt(2,codSala);
+			
+			int esitoInsert = insertTavoloStatement.executeUpdate();
+			if(esitoInsert==1)
+				return true;
+			
+			insertTavoloStatement.close();
+			
+		} catch (SQLException e) {
+			if(e.getErrorCode()==20017) 
+			{
+				JLabel lblErrore = new JLabel("<html>Attenzione: non è possibile aggiungere il nuovo tavolo.<br>"
+						+ "Capienza massima di avventori della sala di riferimento superata!</html>");
+				lblErrore.setFont(new Font("Segoe UI", Font.BOLD, 15));
+				JOptionPane.showMessageDialog(null,lblErrore,"Errore inserimento dati",JOptionPane.ERROR_MESSAGE);	
+			}
+			else 
+			{
+				System.out.println("Codice errore SQL: "+e.getErrorCode()); 
+				System.out.println("SQL State: "+e.getSQLState()); 
+				System.out.println("Messaggio: " +e.getMessage());
+			}
+		}
 		return false;
 	}
 
 	@Override
-	public boolean updateTavolo(int maxAvventori) {
-		// TODO Auto-generated method stub
+	public boolean updateTavolo(int codTavolo, int maxAvventori) {
+		try {
+			String queryUpdateTavolo = "UPDATE Tavolo SET Maxavventori=? WHERE Codtavolo=?";
+			PreparedStatement updateTavoloStatement = connessione.prepareStatement(queryUpdateTavolo);
+			updateTavoloStatement.setInt(1,maxAvventori);
+			updateTavoloStatement.setInt(2,codTavolo);
+			
+			int esitoUpdate = updateTavoloStatement.executeUpdate();
+
+			if(esitoUpdate==1)
+				return true;
+			
+			updateTavoloStatement.close();
+			
+		} catch (SQLException e) {
+			if(e.getErrorCode()==20016) 
+			{
+				JLabel lblErrore = new JLabel("<html>Attenzione: non è possibile modificare il tavolo.<br>"
+						+ "Il valore del numero di avventori per il tavolo deve essere minore o uguale alla capienza della sala!</html>");
+				lblErrore.setFont(new Font("Segoe UI", Font.BOLD, 15));
+				JOptionPane.showMessageDialog(null,lblErrore,"Errore modifica dati",JOptionPane.ERROR_MESSAGE);	
+			}
+			else 
+			{
+				System.out.println("Codice errore SQL: "+e.getErrorCode()); 
+				System.out.println("SQL State: "+e.getSQLState()); 
+				System.out.println("Messaggio: " +e.getMessage());
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean deleteTavolo(int codTavolo) {
-		// TODO Auto-generated method stub
+		try {
+			String queryDeleteTavolo = "DELETE FROM Tavolo WHERE Tavolo.Codtavolo = ?";
+			PreparedStatement deleteTavoloStatement = connessione.prepareStatement(queryDeleteTavolo);
+			deleteTavoloStatement.setInt(1, codTavolo);
+			
+			int esitoDelete = deleteTavoloStatement.executeUpdate();
+			if(esitoDelete==1)
+				return true;
+			
+			deleteTavoloStatement.close();
+			
+		} catch (SQLException e) { 
+			System.out.println("Codice errore SQL: "+e.getErrorCode()); 
+			System.out.println("SQL State: "+e.getSQLState()); 
+			System.out.println("Messaggio: " +e.getMessage());
+		}
 		return false;
 	}
 

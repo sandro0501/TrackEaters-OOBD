fc8e14462d789c0e8c9bb9179f49808eaba12667
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-11 22:58:37 +0200

    definiti metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/TavolataDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/TavolataDAO.java
@@ -1,5 +1,14 @@
 package dao;
 
-public interface TavolataDAO {
+import java.sql.Date;
+import java.util.ArrayList;
 
+import dto.Tavolata;
+
+public interface TavolataDAO {
+	ArrayList<Tavolata> getTavolateByCodTavolo(int codTavolo);
+	int getCodiceTavolataByDataArrivoAndTavolo(String DataArrivo, int codTavolo);
+	boolean insertTavolata(String dataArrivo, int codTavolo, String cameriere);
+	boolean updateTavolata(int codTavolata, String dataArrivo, String cameriere);
+	boolean deleteTavolata(int codTavolata); 
 }

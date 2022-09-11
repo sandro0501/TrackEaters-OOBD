5e3e63a2ed051fbaf5dc23806ece449ccca216e3
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-11 23:01:50 +0200

    completata gestione tavolate (manca collegamento avventori). modifiche minimali ad altre gui

---
+++ OBJECT-ORIENTATION/ProgettoOO/src/gui/AggiungiTavolataFrame.java
@@ -0,0 +1,188 @@
+package gui;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+import controller.Controller;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DefaultEditor;
+import javax.swing.SpinnerNumberModel;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
+import java.util.Calendar;
+
+public class AggiungiTavolataFrame extends JFrame {
+
+	private JPanel pannello_Principale;
+	private JDateChooser campo_DataArrivo;
+	private JTextField campo_OraArrivo;
+	private JTextField campo_OraUscita;
+	private JComboBox campo_Cameriere;
+	private JLabel etichetta_CodTavolo;
+	private Controller theController;
+
+	
+	public AggiungiTavolataFrame(Controller c, boolean isProprietario) {
+		
+		theController = c;
+		
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Modifica_Avventori.class.getResource("/resources/icon.png")));
+		setTitle("TrackEaters - Aggiungi tavolata al tavolo");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 540, 540);
+		setLocationRelativeTo(null);
+		pannello_Principale = new JPanel();
+		pannello_Principale.setBackground(new Color(176, 196, 222));
+		pannello_Principale.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pannello_Principale);
+		pannello_Principale.setLayout(null);
+		
+		JLabel etichetta_Tavolate = new JLabel("");
+		etichetta_Tavolate.setIcon(new ImageIcon(AggiungiTavolataFrame.class.getResource("/resources/aggiungiTavolataTitle.png")));
+		etichetta_Tavolate.setFont(new Font("Tahoma", Font.BOLD, 20));
+		etichetta_Tavolate.setHorizontalAlignment(SwingConstants.CENTER);
+		etichetta_Tavolate.setBounds(17, 28, 490, 52);
+		pannello_Principale.add(etichetta_Tavolate);
+		
+		etichetta_CodTavolo = new JLabel("Tavolo:");
+		etichetta_CodTavolo.setIcon(new ImageIcon(AggiungiRistoranteFrame.class.getResource("/resources/restaurantIcon.png")));
+		etichetta_CodTavolo.setForeground(new Color(0, 0, 128));
+		etichetta_CodTavolo.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		etichetta_CodTavolo.setBounds(20, 91, 487, 27);
+		pannello_Principale.add(etichetta_CodTavolo);
+		
+		JLabel etichetta_DataArrivo = new JLabel("Data arrivo");
+		etichetta_DataArrivo.setIcon(new ImageIcon(AggiungiTavolataFrame.class.getResource("/resources/iconCalendar.png")));
+		etichetta_DataArrivo.setForeground(new Color(0, 0, 128));
+		etichetta_DataArrivo.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		etichetta_DataArrivo.setBounds(20, 141, 223, 27);
+		pannello_Principale.add(etichetta_DataArrivo);
+		
+		JLabel etichetta_OraArrivo = new JLabel("Ora arrivo\r\n");
+		etichetta_OraArrivo.setIcon(new ImageIcon(AggiungiTavolataFrame.class.getResource("/resources/iconOrologio.png")));
+		etichetta_OraArrivo.setForeground(new Color(0, 0, 128));
+		etichetta_OraArrivo.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		etichetta_OraArrivo.setBounds(20, 221, 134, 27);
+		pannello_Principale.add(etichetta_OraArrivo);
+		
+		JLabel etichetta_OraUscita = new JLabel("Ora uscita");
+		etichetta_OraUscita.setIcon(new ImageIcon(AggiungiTavolataFrame.class.getResource("/resources/iconOrologio.png")));
+		etichetta_OraUscita.setForeground(new Color(0, 0, 128));
+		etichetta_OraUscita.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		etichetta_OraUscita.setBounds(247, 221, 134, 27);
+		pannello_Principale.add(etichetta_OraUscita);
+		
+		JLabel etichetta_Cameriere = new JLabel("Cameriere\r\n");
+		etichetta_Cameriere.setIcon(new ImageIcon(AggiungiTavolataFrame.class.getResource("/resources/iconCameriere.png")));
+		etichetta_Cameriere.setForeground(new Color(0, 0, 128));
+		etichetta_Cameriere.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		etichetta_Cameriere.setBounds(20, 271, 172, 27);
+		pannello_Principale.add(etichetta_Cameriere);
+		
+		campo_DataArrivo = new JDateChooser();
+		campo_DataArrivo.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		campo_DataArrivo.setDateFormatString("dd/MM/yyyy");
+		campo_DataArrivo.setBounds(20, 171, 193, 27);
+		pannello_Principale.add(campo_DataArrivo);
+		
+		campo_OraArrivo = new JTextField();
+		campo_OraArrivo.setEditable(false);
+		campo_OraArrivo.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		campo_OraArrivo.setText("20:00:00");
+		campo_OraArrivo.setBounds(140, 225, 86, 27);
+		pannello_Principale.add(campo_OraArrivo);
+		campo_OraArrivo.setColumns(10);
+		
+		campo_OraUscita = new JTextField();
+		campo_OraUscita.setText("22:00:00");
+		campo_OraUscita.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		campo_OraUscita.setEditable(false);
+		campo_OraUscita.setColumns(10);
+		campo_OraUscita.setBounds(367, 225, 86, 27);
+		pannello_Principale.add(campo_OraUscita);
+
+		campo_Cameriere = new JComboBox();
+		campo_Cameriere.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		campo_Cameriere.setSelectedItem("");
+		campo_Cameriere.setBounds(20, 301, 134, 27);
+		pannello_Principale.add(campo_Cameriere);
+		
+		JButton bottone_Annulla = new JButton("");
+		bottone_Annulla.setIcon(new ImageIcon(ImpostazioniProprietarioFrame.class.getResource("/resources/AnnulllaBtn.png")));
+		bottone_Annulla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostraAnnullaDialog(c);
+			}
+		});
+		bottone_Annulla.setBounds(20, 422, 160, 60);
+		pannello_Principale.add(bottone_Annulla);
+		
+		JButton bottone_Aggiungi = new JButton("");
+		JLabel lblAggiungi = new JLabel("Sei sicuro di voler inserire la nuova tavolata?");
+		lblAggiungi.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		bottone_Aggiungi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					if (JOptionPane.showConfirmDialog(pannello_Principale, lblAggiungi)==0) 
+					{
+					
+						DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+						String dataformattata = dateFormat.format(campo_DataArrivo.getDate());
+						c.insertTavolata(isProprietario,dataformattata, campo_Cameriere.getSelectedItem().toString());
+						setVisible(false);
+						c.mostraGestioneTavolateFrame();
+					}
+				}
+		});
+
+		bottone_Aggiungi.setIcon(new ImageIcon(AggiungiRistoranteFrame.class.getResource("/resources/btnAggiungi.png")));
+		bottone_Aggiungi.setBounds(333, 422, 160, 60);
+		pannello_Principale.add(bottone_Aggiungi);
+		
+
+	}
+	
+	private void mostraAnnullaDialog(Controller c) {
+		JLabel lblAnnulla = new JLabel("Sei sicuro di voler annullare l'inserimento della nuova tavolata?");
+		lblAnnulla.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		if (JOptionPane.showConfirmDialog(pannello_Principale, lblAnnulla)==0) {
+			setVisible(false);
+			c.mostraGestioneTavolateFrame();
+		}
+	}
+	
+	public JDateChooser getCampo_DataArrivo() {
+		return campo_DataArrivo;
+	}
+
+	public JComboBox getCampo_Cameriere() {
+		return campo_Cameriere;
+	}
+
+	public JLabel getEtichetta_CodTavolo() {
+		return etichetta_CodTavolo;
+	}
+}
---
+++ OBJECT-ORIENTATION/ProgettoOO/src/gui/GestioneTavolateFrame.java
@@ -0,0 +1,252 @@
+package gui;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import controller.Controller;
+
+public class GestioneTavolateFrame extends JFrame {
+
+	private JPanel pannello_Principale;
+	private Controller theController;
+	private JLabel lblDataEOra;
+	private DefaultTableModel modelloTabella = new DefaultTableModel();
+	private JTable tabellaTavolateRistorante;
+	private JScrollPane scrollPaneTabellaTavolateRistorante;
+		
+	public GestioneTavolateFrame(Controller c, boolean proprietario) {
+		
+		theController = c;
+		
+		setResizable(false);
+		setTitle("TrackEaters - Gestione Sale e Tavolate - Tavolate associate al tavolo");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(HomepageProprietarioFrame.class.getResource("/resources/icon.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		setLocationRelativeTo(null);
+		pannello_Principale = new JPanel();
+		pannello_Principale.setBackground(new Color(176, 196, 222));
+		pannello_Principale.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pannello_Principale);
+		pannello_Principale.setLayout(null);
+		
+		JLabel etichetta_Tavolate = new JLabel("");
+		etichetta_Tavolate.setIcon(new ImageIcon(GestioneTavolateFrame.class.getResource("/resources/tavolateTitle.png")));
+		etichetta_Tavolate.setFont(new Font("Tahoma", Font.BOLD, 20));
+		etichetta_Tavolate.setHorizontalAlignment(SwingConstants.CENTER);
+		etichetta_Tavolate.setBounds(387, 28, 490, 52);
+		pannello_Principale.add(etichetta_Tavolate);
+		
+		JPanel pannello_Navigazione = new JPanel();
+		pannello_Navigazione.setForeground(new Color(0, 0, 128));
+		pannello_Navigazione.setBackground(new Color(176, 196, 222));
+		pannello_Navigazione.setBounds(0, 597, 1264, 84);
+		pannello_Navigazione.setBorder(new LineBorder(new Color(119, 136, 153), 2));
+		pannello_Principale.add(pannello_Navigazione);
+		pannello_Navigazione.setLayout(null);
+		
+		JButton bottone_Home = new JButton("");
+		bottone_Home.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);				
+				c.mostraGestioneRistoranteFrame();
+			}
+		});
+		bottone_Home.setIcon(new ImageIcon(HomepageProprietarioFrame.class.getResource("/resources/btnHome.png")));
+		bottone_Home.setBounds(10, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Home);
+		bottone_Home.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		JButton bottone_Indietro = new JButton("");
+		bottone_Indietro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				c.mostraGestioneTavoliFrame();
+			}
+		});
+		bottone_Indietro.setIcon(new ImageIcon(HomepageProprietarioFrame.class.getResource("/resources/btnIndietro.png")));
+		bottone_Indietro.setFont(new Font("Tahoma", Font.BOLD, 12));
+		bottone_Indietro.setBounds(191, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Indietro);
+		
+		JButton bottone_Logout = new JButton("");
+		bottone_Logout.setIcon(new ImageIcon(HomepageProprietarioFrame.class.getResource("/resources/btnLogout.png")));
+		bottone_Logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostraLogoutDialog(c);
+			}
+		});
+		bottone_Logout.setFont(new Font("Tahoma", Font.BOLD, 12));
+		bottone_Logout.setBounds(1094, 11, 160, 60);
+		pannello_Navigazione.add(bottone_Logout);
+		
+		lblDataEOra = new JLabel("");
+		lblDataEOra.setIcon(new ImageIcon(HomepageProprietarioFrame.class.getResource("/resources/DataEOraIcon.png")));
+		lblDataEOra.setForeground(new Color(0, 0, 128));
+		lblDataEOra.setBounds(420, 11, 429, 60);
+		pannello_Navigazione.add(lblDataEOra);
+		lblDataEOra.setFont(new Font("Segoe UI", Font.BOLD, 25));
+		lblDataEOra.setHorizontalAlignment(SwingConstants.CENTER);
+		c.mostraDataEOra(lblDataEOra);
+		
+		JButton bottone_VisualizzaAvventori = new JButton("");
+		bottone_VisualizzaAvventori.setIcon(new ImageIcon(GestioneTavolateFrame.class.getResource("/resources/btnAvventori.png")));
+		bottone_VisualizzaAvventori.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		
+				if (tabellaTavolateRistorante.getSelectedRow() != -1) {
+					//setVisible(false);
+					//c.startHomepageGestioneRistoranteFrame(true);
+					//c.setHomepageGestioneRistorante(true);
+				} else {
+					c.mostraErroreSelezioneDialog(pannello_Principale);
+				} 
+			
+			}
+		});
+		bottone_VisualizzaAvventori.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_VisualizzaAvventori.setBounds(124, 503, 160, 60);
+		pannello_Principale.add(bottone_VisualizzaAvventori);
+		
+		JButton bottone_Aggiungi = new JButton("");
+		bottone_Aggiungi.setIcon(new ImageIcon(GestioneSaleETavolateFrame.class.getResource("/resources/btnAggiungi.png")));
+		bottone_Aggiungi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					c.startAggiungiTavolataFrame(proprietario);
+					c.riempiCampiAggiungiTavolataPage(proprietario);
+			}
+		});
+		bottone_Aggiungi.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Aggiungi.setBounds(408, 503, 160, 60);
+		pannello_Principale.add(bottone_Aggiungi);
+	
+		
+		JButton bottone_Modifica = new JButton("");
+		bottone_Modifica.setIcon(new ImageIcon(GestioneSaleETavolateFrame.class.getResource("/resources/btnModifica.png")));
+		bottone_Modifica.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (tabellaTavolateRistorante.getSelectedRow()!=-1) {
+					setVisible(false);
+					c.startModificaTavolataFrame(proprietario);
+					c.riempiCampiModificaTavolataPage(proprietario);
+				} else {
+					c.mostraErroreSelezioneDialog(pannello_Principale);
+				}
+				
+				
+			}
+		});
+		bottone_Modifica.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Modifica.setBounds(692, 503, 160, 60);
+		pannello_Principale.add(bottone_Modifica);
+	
+		
+		JLabel lblElimina = new JLabel("<html>Sei sicuro di voler eliminare la tavolata selezionata?<br/>Verranno cancellate tutte le informazioni ad essa collegate.</html>");
+		lblElimina.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		JButton bottone_Elimina = new JButton("");
+		bottone_Elimina.setIcon(new ImageIcon(GestioneSaleETavolateFrame.class.getResource("/resources/btnElimina.png")));
+		bottone_Elimina.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		
+				if (tabellaTavolateRistorante.getSelectedRow()!=-1) {
+					if(JOptionPane.showConfirmDialog(pannello_Principale,lblElimina) == 0) {
+						
+						c.deleteTavolata();
+					}
+				} else {
+					c.mostraErroreSelezioneDialog(pannello_Principale);
+				}
+				
+			}
+		});
+		bottone_Elimina.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		bottone_Elimina.setBounds(976, 503, 160, 60);
+		pannello_Principale.add(bottone_Elimina);
+		
+		
+		setTabellaTavolateRistorante();
+	}
+	
+	private void setTabellaTavolateRistorante() {
+		scrollPaneTabellaTavolateRistorante = new JScrollPane();
+		scrollPaneTabellaTavolateRistorante.setViewportBorder(null);
+		scrollPaneTabellaTavolateRistorante.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPaneTabellaTavolateRistorante.setBounds(10, 119, 1244, 346);
+		scrollPaneTabellaTavolateRistorante.getViewport().setBackground(new Color(176, 196, 222));
+		pannello_Principale.add(scrollPaneTabellaTavolateRistorante);
+		tabellaTavolateRistorante = new JTable();
+		tabellaTavolateRistorante.setForeground(new Color(0, 0, 128));
+		tabellaTavolateRistorante.setBackground(Color.WHITE);
+		tabellaTavolateRistorante.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		modelloTabella.addColumn("Num.");
+		modelloTabella.addColumn("Tavolo");
+		modelloTabella.addColumn("Data arrivo");
+		modelloTabella.addColumn("Ora arrivo");
+		modelloTabella.addColumn("Ora uscita");
+		modelloTabella.addColumn("Cameriere");
+		tabellaTavolateRistorante.setModel(modelloTabella);
+		tabellaTavolateRistorante.getTableHeader().setAlignmentX(CENTER_ALIGNMENT);
+		tabellaTavolateRistorante.getTableHeader().setBackground(new Color(0, 0, 128));
+		tabellaTavolateRistorante.getTableHeader().setForeground(Color.WHITE);
+		tabellaTavolateRistorante.getTableHeader().setFont(new Font("Segoe UI", Font.BOLD, 15));
+		tabellaTavolateRistorante.getTableHeader().setReorderingAllowed(false);
+		tabellaTavolateRistorante.setSelectionBackground(new Color(245, 245, 220));
+		tabellaTavolateRistorante.setRowHeight(30);
+		tabellaTavolateRistorante.setDefaultEditor(Object.class, null);
+		tabellaTavolateRistorante.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		scrollPaneTabellaTavolateRistorante.setViewportView(tabellaTavolateRistorante);
+		
+	}
+	
+	private void mostraLogoutDialog(Controller c) {
+		JLabel lblLogout = new JLabel("Sei sicuro di voler uscire?");
+		lblLogout.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		if(JOptionPane.showConfirmDialog(pannello_Principale, lblLogout)==0) {
+			setVisible(false);
+			c.startLoginFrame();
+		}
+	}
+	
+	public DefaultTableModel getModel() {
+		return modelloTabella;
+	}
+	
+	public void setModel(DefaultTableModel model) {
+		this.modelloTabella = model;
+	}
+
+	public JTable getTabellaTavolateRistorante() {
+		return tabellaTavolateRistorante;
+	}
+
+	
+
+}

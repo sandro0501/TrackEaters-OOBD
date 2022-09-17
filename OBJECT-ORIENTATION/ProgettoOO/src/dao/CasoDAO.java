cf4fda3d28f551dfef2466ec43cfd564bf67ce77
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-18 01:24:45 +0200

    aggiunti nuovi metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/CasoDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/CasoDAO.java
@@ -7,5 +7,6 @@ public interface CasoDAO {
 	
 	ArrayList<Caso> getCasiCovidByCodRistorante(int codRistorante); 
 	boolean insertCaso(String dataRegistrazione,String numCid,String stato,String note);
-
+	boolean updateCaso(int codiceCaso,String dataRegistrazione,String numCid,String stato,String note);
+	boolean deleteCaso(int codiceCaso);
 }

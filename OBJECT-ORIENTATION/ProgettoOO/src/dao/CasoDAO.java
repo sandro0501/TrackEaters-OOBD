0e68cf360716c0f629c495d19faefd6dcadf08d4
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-17 02:17:56 +0200

    aggiunti primi metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/dao/CasoDAO.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/dao/CasoDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import java.util.ArrayList;
+import dto.Caso;
+
 public interface CasoDAO {
+	
+	ArrayList<Caso> getCasiCovidByCodRistorante(int codRistorante); 
+	boolean insertCaso(String dataRegistrazione,String numCid,String stato,String note);
 
 }

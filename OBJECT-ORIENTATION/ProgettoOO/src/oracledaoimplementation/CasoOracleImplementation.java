d4c903151c8a9cbbc48006451aabc18e6e999ccc
SantoloBarretta <san.barretta@studenti.unina.it>
2022-09-18 01:26:31 +0200

    implementazione metodi dao

--- OBJECT-ORIENTATION/ProgettoOO/src/oracledaoimplementation/CasoOracleImplementation.java
+++ OBJECT-ORIENTATION/ProgettoOO/src/oracledaoimplementation/CasoOracleImplementation.java
@@ -34,14 +34,14 @@ public class CasoOracleImplementation implements CasoDAO {
 		ArrayList<Caso> casi = new ArrayList<Caso>();
 		
 		try {
-			String queryGetCasi = "SELECT DISTINCT C.DataRegistrazione,C.AvventorePositivo,C.Statocaso,C.Note FROM CASO C JOIN ACCOGLIENZA A ON "
+			String queryGetCasi = "SELECT DISTINCT C.Codcaso,C.DataRegistrazione,C.AvventorePositivo,C.Statocaso,C.Note FROM CASO C JOIN ACCOGLIENZA A ON "
 					+ "C.AvventorePositivo = A.Avventore WHERE A.Ristorante=? ORDER BY C.DataRegistrazione";
 			PreparedStatement stmtGetCasi = connessione.prepareStatement(queryGetCasi);
 			stmtGetCasi.setInt(1, codRistorante);
 			ResultSet rs = stmtGetCasi.executeQuery();
 			
 			while(rs.next()) {
-				Caso c = new Caso(rs.getDate(1),rs.getString(2),rs.getString(3),rs.getString(4));
+				Caso c = new Caso(rs.getInt(1),rs.getDate(2),rs.getString(3),rs.getString(4),rs.getString(5));
 				casi.add(c);
 			}		
 			rs.close();
@@ -71,6 +71,53 @@ public class CasoOracleImplementation implements CasoDAO {
 			insertCasoStatement.close();
 			
 		} catch (SQLException e) {
+			System.out.println("Codice errore SQL: "+e.getErrorCode()); 
+			System.out.println("SQL State: "+e.getSQLState()); 
+			System.out.println("Messaggio: " +e.getMessage());
+		}
+		return false;
+	}
+
+	@Override
+	public boolean updateCaso(int codiceCaso,String dataRegistrazione, String numCid, String stato, String note) {
+		try {
+			String queryUpdateCaso = "UPDATE caso SET dataregistrazione=TO_DATE(?,'dd/mm/yyyy'),statocaso=?,note=?,avventorepositivo=? WHERE codcaso=?";
+			PreparedStatement updateCasoStatement = connessione.prepareStatement(queryUpdateCaso);
+			updateCasoStatement.setString(1, dataRegistrazione);
+			updateCasoStatement.setString(2, stato);
+			updateCasoStatement.setString(3, note);
+			updateCasoStatement.setString(4, numCid);
+			updateCasoStatement.setInt(5, codiceCaso);
+			
+			int esitoUpdate = updateCasoStatement.executeUpdate();
+
+			if(esitoUpdate==1)
+				return true;
+			
+			updateCasoStatement.close();
+			
+		} catch (SQLException e) {
+			System.out.println("Codice errore SQL: "+e.getErrorCode()); 
+			System.out.println("SQL State: "+e.getSQLState()); 
+			System.out.println("Messaggio: " +e.getMessage());
+		}
+		return false;
+	}
+
+	@Override
+	public boolean deleteCaso(int codiceCaso) {
+		try {
+			String queryDeleteCaso = "DELETE FROM Caso WHERE Codcaso = ?";
+			PreparedStatement deleteCasoStatement = connessione.prepareStatement(queryDeleteCaso);
+			deleteCasoStatement.setInt(1, codiceCaso);
+			
+			int esitoDelete = deleteCasoStatement.executeUpdate();
+			if(esitoDelete==1)
+				return true;
+			
+			deleteCasoStatement.close();
+			
+		} catch (SQLException e) { 
 			System.out.println("Codice errore SQL: "+e.getErrorCode()); 
 			System.out.println("SQL State: "+e.getSQLState()); 
 			System.out.println("Messaggio: " +e.getMessage());
